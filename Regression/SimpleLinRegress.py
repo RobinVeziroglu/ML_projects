44e45febc7d1f272d09dd00714b735f00ac58e02
Veziroglu Robin <Robin.Veziroglu@volvo.com>
2022-02-22 12:41:25 +0100

    Small changes to code

--- Regression/SimpleLinRegress.py
+++ Regression/SimpleLinRegress.py
@@ -15,7 +15,7 @@ path_test="C:\\Users\\A403922\\Github_Demo\\Project_1\\Regression\\test.csv"
 df_train=pd.read_csv(path_train)
 df_test=pd.read_csv(path_test)
 
-#Quick data-cleaning
+#Data-cleaning
 #Are there rows with missing values?
 #print(df_train.isnull().any(axis = 1).sum())
 #We have 1 row with a missing value
@@ -27,7 +27,7 @@ df_train=df_train.dropna(how='any',axis=0)
 #to predict the response variably y.
 #y_i=beta_0+beta_1*x_i
 
-#quick overview/visualization of the data
+#Quick overview/visualization of the data
 #df_train.describe()
 #plt.scatter(df_train.x,df_train.y)
 #exhibits a positive correlation
@@ -49,11 +49,20 @@ print('Estimated y-intercept for the regression-line is {}'.format(intercept))
 slope=model.coef_
 print('Estimated slope for the regression line is {}'.format(slope))
 
+#visualization of the predicted regression-line compared to the original data
+'''
+plt.scatter(x, y, color='blue')
+plt.plot(x, model.predict(x),color='yellow')
+plt.xlabel('x_train')
+plt.ylabel('y_train')
+plt.show()
+'''
+
 x_test=np.array([df_test.x.to_list()]).reshape(-1,1)
 y_test=np.array([df_test.y.to_list()]).reshape(-1,1)
 
 y_pred=model.predict(x_test)
 #alternative to above: y_pred=intercept+slope*x_test
-plt.scatter(x_test,y_test)
+#plt.scatter(x_test,y_test)
 
 
