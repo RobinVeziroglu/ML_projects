daf449fdfde15ab165d9545327463c8976b77c7e
Veziroglu Robin <Robin.Veziroglu@volvo.com>
2022-02-22 11:26:05 +0100

    Adding the .py file

---
+++ Regression/lin-regress.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 11:14:26 2022
+
+@author: a403922
+"""
+
+import pandas  as pd 
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns 
